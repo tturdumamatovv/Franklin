d55507ab9e7772275a72234c2152b498204d79fe
tturdumamatovv <muki2022@mail.ru>
2024-04-13 11:57:27 +0600

    gunicorn

--- config/settings.py
+++ config/settings.py
@@ -147,7 +147,7 @@ LANGUAGES = (
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
