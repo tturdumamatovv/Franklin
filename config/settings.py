334111220a47e53dab454552a2e79e9659c4bdd1
tturdumamatovv <muki2022@mail.ru>
2024-04-13 14:36:47 +0600

    static

--- config/settings.py
+++ config/settings.py
@@ -146,8 +146,8 @@ LANGUAGES = (
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
