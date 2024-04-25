e0e987e617b17078a6dad28005b4622f4fa98cba
tturdumamatovv <muki2022@mail.ru>
2024-04-25 16:37:06 +0600

    a

--- apps/franklin/serializers.py
+++ apps/franklin/serializers.py
@@ -1,15 +1,15 @@
 from rest_framework import serializers
 
 from apps.contacts.models import Preload
-from apps.portfolio.models import PortfolioPage
 from apps.services.models import ServicePage
 from apps.contacts.models import Contact
 from apps.about_us.models import AboutPage
 
+
 from apps.about_us.serializers import AboutPageSerializer
 from apps.contacts.serializers import ContactSerializer
 from apps.portfolio.serializers import PortfolioPage, PortfolioDurationSerializer
-from apps.services.serializers import ServiceSerializer, ServicePageSerializer
+from apps.services.serializers import ServicePageSerializer
 
 
 class AllPagesSerializer(serializers.Serializer):
@@ -20,19 +20,20 @@ class AllPagesSerializer(serializers.Serializer):
 
     def get_about_page(self, obj):
         instance = AboutPage.objects.first()
-        return AboutPageSerializer(instance).data if instance else None
+        return AboutPageSerializer(instance, context=self.context).data if instance else None
 
     def get_portfolio_page(self, obj):
         instance = PortfolioPage.objects.first()
-        return PortfolioDurationSerializer(instance).data if instance else None
+        return PortfolioDurationSerializer(instance, context=self.context).data if instance else None
 
     def get_service_page(self, obj):
         instance = ServicePage.objects.first()
-        return ServicePageSerializer(instance).data if instance else None
+        return ServicePageSerializer(instance, context=self.context).data if instance else None
 
     def get_contact_page(self, obj):
         instance = Contact.objects.first()
-        return ContactSerializer(instance).data if instance else None
+        return ContactSerializer(instance, context=self.context).data if instance else None
+
 
 
 class PreloadSerializer(serializers.ModelSerializer):
